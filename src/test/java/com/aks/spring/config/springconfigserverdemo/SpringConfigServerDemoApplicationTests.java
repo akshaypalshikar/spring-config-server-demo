e2aecb9d6a401fd2a5df94053bf93c8144e356de
Akshay Palshikar <aksmakesit@gmail.com>
2022-04-03 16:43:11 +0530

    initial commit

---
+++ src/test/java/com/aks/spring/config/springconfigserverdemo/SpringConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aks.spring.config.springconfigserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
