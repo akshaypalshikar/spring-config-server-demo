b1dfcf7304433debc718d2fb848c4436653f3c8c
Akshay Palshikar <aksmakesit@gmail.com>
2022-04-03 17:13:44 +0530

    adding reference to git repo for reading configs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-config-server-demo
+Try urls :
+* http://localhost:8080/application/default/
+* http://localhost:8080/notification-demo/default
--- src/main/java/com/aks/spring/config/springconfigserverdemo/SpringConfigServerDemoApplication.java
+++ src/main/java/com/aks/spring/config/springconfigserverdemo/SpringConfigServerDemoApplication.java
@@ -2,8 +2,10 @@ package com.aks.spring.config.springconfigserverdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigServerDemoApplication {
 
 	public static void main(String[] args) {
